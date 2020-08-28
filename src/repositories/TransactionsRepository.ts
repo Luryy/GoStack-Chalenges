db6ce9016ae00c79b793af4cb803b49baaaeeed2
Lury <lucasyuri56@hotmail.com>
2020-08-27 22:06:06 -0300

    finish chalenges

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import { EntityRepository, Repository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
@@ -11,7 +12,36 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const allTransactions = await this.find();
+
+    const { income, outcome } = allTransactions.reduce(
+      (current, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            current.income += transaction.value;
+            break;
+          case 'outcome':
+            current.outcome += transaction.value;
+            break;
+
+          default:
+            break;
+        }
+        return current;
+      },
+      {
+        income: 0,
+        outcome: 0,
+      },
+    );
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 }
 
