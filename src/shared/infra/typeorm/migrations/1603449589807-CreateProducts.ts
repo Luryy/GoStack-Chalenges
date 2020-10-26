8898915ad36ce541d3defbf9c262e734be17326a
Lucas Yuri <lucasyuri56@hotmail.com>
2020-10-26 19:59:34 -0300

    chore: add scale to price

--- src/shared/infra/typeorm/migrations/1603449589807-CreateProducts.ts
+++ src/shared/infra/typeorm/migrations/1603449589807-CreateProducts.ts
@@ -20,6 +20,8 @@ export default class CreateProducts1603449589807 implements MigrationInterface {
           {
             name: 'price',
             type: 'decimal',
+            precision: 6,
+            scale: 2,
           },
           {
             name: 'quantity',
